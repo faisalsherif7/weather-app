ff918b562d5a43b6ab0f36c540ef0aec9eb383fc
Faisal Sherif <116718026+faisalsherif7@users.noreply.github.com>
2024-08-18 17:51:30 +0530

    Write function that hits API and returns data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+async function getWeather(location) {
+    const weatherData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=6GGY5DHYK4ZV5PE82LWWSRBFL`)
+    const dataJson = await weatherData.json()
+    return dataJson
+}
+
+const data = getWeather('kyiv')
+console.log(data)
