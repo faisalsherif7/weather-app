defb6addd245e2a83e7fb6ebab880b86667daba8
Faisal Sherif <116718026+faisalsherif7@users.noreply.github.com>
2024-08-18 18:11:53 +0530

    Set up form to imput location

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import './style.css';
 async function getWeather(location) {
     const weatherData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=6GGY5DHYK4ZV5PE82LWWSRBFL`)
     const dataJson = await weatherData.json()
-    console.log(dataJson.currentConditions)
+    console.log(dataJson)
 }
 
-getWeather('kyiv')
+document.querySelector('button').addEventListener('click', () => {
+    const value = document.querySelector('input').value
+    getWeather(value)
+})
