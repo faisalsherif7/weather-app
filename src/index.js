e7a346c39df7b5ec64d66e027d387ab93f111020
Faisal Sherif <116718026+faisalsherif7@users.noreply.github.com>
2024-08-19 06:43:25 +0530

    Style location and temperature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 async function getWeather(location) {
-    const weatherData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=6GGY5DHYK4ZV5PE82LWWSRBFL`)
+    const weatherData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=6GGY5DHYK4ZV5PE82LWWSRBFL&unitGroup=metric`)
     const dataJson = await weatherData.json()
     console.log(dataJson)
 }
