b63ef1e44da1025c62646ca9fcaee0bcdd1f1a61
Faisal Sherif <116718026+faisalsherif7@users.noreply.github.com>
2024-08-22 22:29:58 +0530

    Build out dist folder

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import globals, { node } from "globals";
-import pluginJs from "@eslint/js";
-
-
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
